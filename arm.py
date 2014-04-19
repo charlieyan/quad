6583f936a9da955cb1e762c744976f498c1df05e
Charlie Yan <charlieyan08@gmail.com>
2014-04-19 11:20:43 -0400

    arm

--- arm.py
+++ arm.py
@@ -1,14 +1,10 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-from bbio import *
-import time
+import ctypes, time
+libc = ctypes.CDLL('libc.so.6')
 
 #CONSTANTS
-ESC1PIN "P9_11"
-ESC2PIN "P9_12"
-ESC3PIN "P9_13"
-ESC4PIN "P9_14"
-PULSETIME 1100
+PULSETIME = 1100
 
 def setupESC(pin):
   # set pin as OUTPUT
@@ -23,31 +19,13 @@ def setupESC(pin):
     #time.sleep(8.9/1000) #8.9 milliseconds
   print "Done arming " + pin
   return
-
-def test():
-  GPIO.setup("P9_12",GPIO.OUT)
-  for i in range(0,20):
-    GPIO.output("P9_12",GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output("P9_12",GPIO.LOW)
-    time.sleep(1)
-  GPIO.cleanup()
-
-def loop():
-  pulseTime = 1200
-  GPIO.output(ESC1PIN,GPIO.HIGH)
-  delayMicroseconds(pulseTime)
-  GPIO.output(ESC1PIN,GPIO.LOW)
-  delay(10-(pulseTime/1000))
   
-def setup():
-  setupESC(ESC1PIN)
+def setupGlobalPins():
+  global ESC1PIN = "P9_11"
+  global ESC2PIN = "P9_12"
+  global ESC3PIN = "P9_13"
+  global ESC4PIN = "P9_14"
 
 def main():
+  setupGlobalPins()
   setupESC(ESC1PIN)
-  time.sleep(4)
-  print "Starting drive"
-  GPIO.cleanup()
-
-if __name__ == '__main__':
-  run(setup,loop)  
