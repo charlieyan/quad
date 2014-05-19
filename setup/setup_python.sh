0f7a59d76ea6fa2845dcb5e068e389bfadad47ed
Charlie Yan <charlieyan08@gmail.com>
2014-05-19 00:58:32 +0000

    setup files for spi

--- setup/setup_python.sh
+++ setup/setup_python.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt-get install build-essential python-dev python-pip -y
 #easy_install -U distribute  //debian only
 sudo pip install Adafruit_BBIO
+sudo pip install spidev
